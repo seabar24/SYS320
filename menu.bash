96cdb3526e7e9fe1e2a577796a40446edb737d66
seabar24 <71162295+seabar24@users.noreply.github.com>
2023-09-19 13:54:32 -0400

    Update menu.bash

--- menu.bash
+++ menu.bash
@@ -176,7 +176,7 @@ function users_menu() {
 		# Checks if User ID is 0 or not.
 		 read -p "What user do you want to check? " user
 
-		 if grep "$user" /etc/passwd | cut -d: -f3 == 0 ; then
+		 if grep "$user" /etc/passwd | cut -d: -f3 ; then
 
 		 	echo ""
 		 	echo "User $user has a UID of 0."
