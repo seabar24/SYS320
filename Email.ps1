e236e1d5c9c77cecb509a914d5514c0581c03234
seabar24 <sean.barrick@mymail.champlain.edu>
2023-10-17 13:40:00 -0400

    Email script (doesn't work)

---
+++ Email.ps1
@@ -0,0 +1,15 @@
+﻿# Storyline: Send an email.
+
+# Variable can have an underscore or any alphanumeric value.
+
+# Body of the email
+$msg = "Hello there."
+
+write-host -BackgroundColor Red -ForegroundColor white $msg
+
+# Email From Address
+$email = "sean.barrick@mymail.champlain.edu"
+
+# To address
+$toEmail = "deployer@csi-web"
+Send-MailMessage -From $email -to $toEmail -Subject "A Greeting" -body $msg -SmtpServer 192.168.6.71
