58cff41f126e8256b8ae0398853ff49685bfb919
seabar24 <sean.barrick@mymail.champlain.edu>
2023-10-24 13:51:12 -0400

    80 percent of Week 9

---
+++ WMI.ps1
@@ -0,0 +1,13 @@
+﻿# Use the Get-WMIobject cmdlet
+# Get-WMiobject -Class win32_service | select Name, PathName, ProcessId
+
+# get-wmiobject -list | where { $_.Name -ilike "win32_[n-z]*" } | sort-object
+
+# Get-WmiObject -Class Win32_Account | Get-Member
+
+# Task: Grab the network adapter information using the WMI class
+# Get the IP Address, default gateway, and the DNS servers.
+# Bonus: get the DHCP server.
+# Post your code to pineapple
+# Running your code using a screen recorder.
+
---
+++ process_n_service.ps1
@@ -0,0 +1,5 @@
+﻿# Storyline: Using the Get-Process and Get-service
+# Get-Process | Select-Object Path, ProcessName, ID | 
+# Export-Csv -Path "C:\Users\sbarrick\Desktop\myProcesses.csv" -NoTypeInformation
+# Get-Process | Get-Member
+# Get-Service | where { $_.Status -eq "Stopped" }
