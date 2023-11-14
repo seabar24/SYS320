13181e3ac911181e555a07a61ab7bc5c95d7f5a3
seabar24 <sean.barrick@mymail.champlain.edu>
2023-11-14 13:09:37 -0500

    Beginning of Week 12 code

---
+++ sshserver.ps1
@@ -0,0 +1,18 @@
+﻿# Storyline: Login to a remote SSH Server
+# New-SSHSession -ComputerName '192.168.4.22' -Credential (Get-Credential sys320)
+
+<#
+while ($True) {
+
+# Add a prompt to run commands
+$the_cmd = read-host -Prompt "Please enter a command"
+
+
+# Run command on remote SSH Server
+(Invoke-SSHCommand -index 0 'ps -ef').Output
+
+}
+#>
+
+Set-SCPFile -ComputerName '192.168.4.22' -Credential (Get-Credential sys320) `
+-RemotePath 'home/sys320' -LocalFile '.\tedx.jpeg'
